440de32dd74c719e8a68d7788cabdd65b667359b
johnaagelv <j.andersen.lv@gmail.com>
2025-07-25 22:39:58 +0300

    Defining redjack17 tiles as names, ongoing

---
+++ generate/tiles.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+tile_unknown	= 0
+actor_fighter	= 9786
+actor_warrior	= 9787
+shape_heart		= 9829
+shape_diamond	= 9830
+bush			= 9827
+mushroom		= 9824
+stones			= 8226
+square_hole		= 9688
+sink			= 9675
+square_ring		= 9689
+bag				= 9794
+beet_root		= 9792
+music_note		= 9834
+duck			= 9835
+helmet			= 9788
+dir_right		= 9658
+dir_left		= 9668
+arrow_up_down	= 8597
+gate			= 8252
+pot				= 182
+rope			= 167
