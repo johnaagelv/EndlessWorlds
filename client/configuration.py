2c16c53fb00db45cf53727a3214cdb8b48e14878
johnaagelv <j.andersen.lv@gmail.com>
2025-11-30 18:02:41 +0200

    Defined UI layout specifications

--- client/configuration.py
+++ client/configuration.py
@@ -9,10 +9,53 @@ LOG_FILENAME_CLIENT = Path("EWclient.log")
 LOG_LEVEL_CLIENT = INFO
 CONSOLE_WIDTH = 80
 CONSOLE_HEIGHT = 50
-VIEW_PORT_WIDTH = 50
-VIEW_PORT_HEIGHT = 45
 GAME_HOST = "192.168.1.104"
 GAME_PORT = 25261
+
+""" Player configuration """
+
+"""
+Player states, where:
+- name, used for presenting
+- value, current value of the state
+- max, maximum value of the state
+- usage, use of the state per tick
+"""
+PLAYER_STATES = [
+	["Health", 500000, 1000000, -1],
+	["Energy", 500000, 1000000, -1]
+]
+
+"""
+UI layout - 1: Map view port; 2: Message view port; 3: World and player states view port
++------------+--+
+|            |  |
+|     1      |  |
+|            | 3|
++------------+  |
+|     2      |  |
++------------+--+
+"""
+# View port for the map presentation 50 x 45
+VIEW_PORT_X = 0
+VIEW_PORT_Y = 0
+VIEW_PORT_WIDTH = CONSOLE_WIDTH - 20
+VIEW_PORT_HEIGHT = CONSOLE_HEIGHT - 5
+# Player states 28 x 6 giving space to 6 states (Health, Energy, ...)
+STATE_PORT_X = VIEW_PORT_WIDTH + 1
+STATE_PORT_Y = 2
+STATE_PORT_WIDTH = CONSOLE_WIDTH - VIEW_PORT_WIDTH - 2
+STATE_PORT_HEIGHT = 6
+# World port for the world and map information
+WORLD_PORT_X = VIEW_PORT_WIDTH + 1
+WORLD_PORT_Y = 0
+WORLD_PORT_WIDTH = CONSOLE_WIDTH - VIEW_PORT_WIDTH - 2
+WORLD_PORT_HEIGHT = 1
+# Message port for the in game messages
+MESSAGE_PORT_X = 1
+MESSAGE_PORT_Y = VIEW_PORT_HEIGHT + 1
+MESSAGE_PORT_WIDTH = VIEW_PORT_WIDTH - 2
+MESSAGE_PORT_HEIGHT = CONSOLE_HEIGHT - MESSAGE_PORT_Y
 
 """ General configuration """
 APP_TITLE = "Endless Worlds, 2025 - A multiplayer multiworld roguelike"
