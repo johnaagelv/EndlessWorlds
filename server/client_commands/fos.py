537ce2f12b091f0ab359219e412b1acd193635f1
johnaagelv <j.andersen.lv@gmail.com>
2025-12-01 22:20:27 +0200

    Added updating actor list

--- server/client_commands/fos.py
+++ server/client_commands/fos.py
@@ -3,6 +3,13 @@ from __future__ import annotations
 from worlds.world import TWorld
 
 def cmd_fos(request: dict, world: TWorld) -> dict:
+	world.actors[request["cid"]] = {
+		"x": int(request["x"]),
+		"y": int(request["y"]),
+		"z": 0,
+		"m": int(request["m"])
+	}
+
 	response = world.get_map_field_of_sense(request['m'], request['x'], request['y'], request['r'])
 	response["cid"] = request["cid"]
 	response["cmd"] = request["cmd"]
