8f023a2908ec42cc32c5ae44723c6ae841ec5df6
johnaagelv <j.andersen.lv@gmail.com>
2025-12-06 19:54:37 +0200

    Added face property to the actors list

--- server/client_commands/fos.py
+++ server/client_commands/fos.py
@@ -6,8 +6,8 @@ def cmd_fos(request: dict, world: TWorld) -> dict:
 	world.actors[request["cid"]] = {
 		"x": int(request["x"]),
 		"y": int(request["y"]),
-		"z": 0,
-		"m": int(request["m"])
+		"m": int(request["m"]),
+		"face": request["face"],
 	}
 
 	response = world.get_map_field_of_sense(request['m'], request['x'], request['y'], request['r'])
