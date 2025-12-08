e2939845ea88e6c7d84a97c5fef1902f95024dcc
johnaagelv <j.andersen.lv@gmail.com>
2025-12-08 17:58:32 +0200

    FOS updates clients in a different way

--- server/client_commands/fos.py
+++ server/client_commands/fos.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 from worlds.world import TWorld
 
 def cmd_fos(request: dict, world: TWorld) -> dict:
-	world.actors[request["cid"]] = {
-		"x": int(request["x"]),
-		"y": int(request["y"]),
-		"m": int(request["m"]),
-		"face": request["face"],
-	}
+	world.actors[request["cid"]] |= request
+
+#	world.actors[request["cid"]] = {
+#		"x": int(request["x"]),
+#		"y": int(request["y"]),
+#		"m": int(request["m"]),
+#		"face": request["face"],
+#	}
 
 	response = world.get_map_field_of_sense(request['m'], request['x'], request['y'], request['r'])
 	response["cid"] = request["cid"]
