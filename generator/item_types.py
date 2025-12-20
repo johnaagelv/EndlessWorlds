09a220a1b966a5fb4ffa9b1ad96ec67da702aa59
johnaagelv <j.andersen.lv@gmail.com>
2025-12-20 17:54:42 +0200

    Redefined item definition for easy usage

--- generator/item_types.py
+++ generator/item_types.py
@@ -1,25 +1,17 @@
 from __future__ import annotations
-import numpy as np
+
 from typing import Tuple
 import generator.colours as colour
-
-item_dt = np.dtype(
-	[
-		("face", np.int32), # Face of this item, unicode
-		("fg", "4B"), # Foreground colour, RGBA
-		("bg", "4B"), # Background colour, RGBA
-	]
-)
 
 items: dict = {}
 
 def new_item(
-	*,
+	name: str,
 	face: int,
 	fg: Tuple[int, int, int, int],
-	bg: Tuple[int, int, int, int]
-) -> np.ndarray:
-	return np.array((face, fg, bg), dtype=item_dt)
+	bg: Tuple[int, int, int, int],
+) -> dict:
+	return {"name": name, "face": face, "fg": fg, "bg": bg}
 
 item_types: list = [
 	"consumables", # food parcel, water, fruit, vegetables, berries, meat, ...
@@ -31,12 +23,14 @@ item_types: list = [
 ]
 
 items["food parcel"] = new_item(
-	face=9576,
-	fg=(255, 255, 255, 255),
-	bg=(237, 237, 238, 192)
+	"Food parcel",
+	9576,
+	colour.food_parcel,
+	colour.background,
 )
 items["backpack"] = new_item(
-	face=162,
-	fg=colour.food_parcel,
-	bg=colour.background
+	"Backpack",
+	162,
+	colour.backpack,
+	colour.background,
 )
